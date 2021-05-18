622867aa821f78889375b9e1bb47acca7f2bc75a
leocostaa <32634126+leocostaa@users.noreply.github.com>
2021-05-18 23:55:40 +0100

    Criação do BdOpenHelper

---
+++ app/src/main/java/com/example/mycovid19app/BdAppOpenHelper.kt
@@ -0,0 +1,23 @@
+package com.example.mycovid19app
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BdAppOpenHelper (context : Context?)  :
+    SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        if (db != null) {
+            //TabelaPaciente(db).cria()
+
+        }
+    }
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+    companion object{
+        const val NOME_BASE_DADOS = "CovidApp.db"
+        const val VERSAO_BASE_DADOS = 1
+    }
+}
