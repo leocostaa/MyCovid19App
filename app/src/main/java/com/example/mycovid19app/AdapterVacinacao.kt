7e2716ae7ee0c43282bc336d52021f3efd567afb
leocostaa <32634126+leocostaa@users.noreply.github.com>
2021-06-23 17:00:59 +0100

    Notificar o Adapter Vacinacao que os dados foram alterados(cada vez que forem)

--- app/src/main/java/com/example/mycovid19app/AdapterVacinacao.kt
+++ app/src/main/java/com/example/mycovid19app/AdapterVacinacao.kt
@@ -6,8 +6,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterVacinacao(val fragment: FragmentVacinacao,var cursor: Cursor? = null) : RecyclerView.Adapter<AdapterVacinacao.ViewHolderVacinacao>(){
+class AdapterVacinacao(val fragment: FragmentVacinacao) : RecyclerView.Adapter<AdapterVacinacao.ViewHolderVacinacao>(){
 
+    public var cursor: Cursor? = null
+        get() = field
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+    
     class ViewHolderVacinacao(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val textViewDataVac = itemView.findViewById<TextView>(R.id.textViewDataVac)
 
