e2d90576c3f584c0d02197785b86ce1e01f8a77f
leocostaa <32634126+leocostaa@users.noreply.github.com>
2021-06-24 17:35:39 +0100

    Configurar o Recycler view para os locais

--- app/src/main/java/com/example/mycovid19app/FragmentLocal.kt
+++ app/src/main/java/com/example/mycovid19app/FragmentLocal.kt
@@ -10,12 +10,14 @@ import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mycovid19app.databinding.FragmentLocalBinding
 
 class FragmentLocal : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
     private var _binding: FragmentLocalBinding? = null
+    private var adapterLocais : AdapterLocais? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,8 +34,11 @@ class FragmentLocal : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerViewLocais = view.findViewById<RecyclerView>(R.id.recyclerViewLocal)
-        //recyclerViewLivros.adapter
+        val recyclerViewLocal = view.findViewById<RecyclerView>(R.id.recyclerViewLocal)
+        adapterLocais = AdapterLocais() 
+        recyclerViewLocal.adapter = adapterLocais
+        recyclerViewLocal.layoutManager = LinearLayoutManager(requireContext())
+
 
         LoaderManager.getInstance(this)
             .initLoader(ID_LOADER_MANAGER_LOCAIS, null, this)
