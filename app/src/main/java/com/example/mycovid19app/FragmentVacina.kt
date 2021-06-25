e276468d3b0c00c160ebd0e32cadac1f821025bc
leocostaa <32634126+leocostaa@users.noreply.github.com>
2021-06-25 18:03:36 +0100

    Iniciado um CursorLoader para ler as vacinas em background

--- app/src/main/java/com/example/mycovid19app/FragmentVacina.kt
+++ app/src/main/java/com/example/mycovid19app/FragmentVacina.kt
@@ -1,15 +1,19 @@
 package com.example.mycovid19app
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 import androidx.navigation.fragment.findNavController
 import com.example.mycovid19app.databinding.FragmentVacinaBinding
 
 
-class FragmentVacina : Fragment() {
+class FragmentVacina : Fragment(),LoaderManager.LoaderCallbacks<Cursor>{
 
     private var _binding: FragmentVacinaBinding? = null
 
@@ -23,6 +27,7 @@ class FragmentVacina : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+
         _binding = FragmentVacinaBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -31,12 +36,35 @@ class FragmentVacina : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        LoaderManager.getInstance(this)
+            .initLoader(ID_LOADER_MANAGER_VACINAS, null, this)
 //            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(
+            requireContext(),
+            ContentProviderApp.ENDERECO_VACINA,
+            TabelaVacina.TODAS_COLUNAS,
+            null, null,
+            TabelaVacina.CAMPO_ORIGEM
+        )
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
+    }
+
+    companion object{
+        const val ID_LOADER_MANAGER_VACINAS = 0
     }
 }
