af0073ad34e1fa7cb9331795774174652fbafa0c
leocostaa <32634126+leocostaa@users.noreply.github.com>
2021-06-30 18:46:06 +0100

    Criado o menu para locais; ajustes no layout local

--- app/src/main/java/com/example/mycovid19app/MainActivity.kt
+++ app/src/main/java/com/example/mycovid19app/MainActivity.kt
@@ -68,6 +68,7 @@ class MainActivity : AppCompatActivity() {
                 R.menu.menu_pacientes -> (DadosApp.fragment as FragmentPacientes).processaOpcaoMenu(item)
                 R.menu.menu_novo_paciente -> (DadosApp.fragment as FragmentNovoPaciente).processaOpcaoMenu(item)
                 R.menu.menu_inicio_page ->(DadosApp.fragment as FragmentInicioPage).processaOpcaoMenu(item)
+               // R.menu.menu_locais ->(DadosApp.fragment as FragmentLocal).processaOpcaoMenu(item)
                 else -> false
             }
         }
